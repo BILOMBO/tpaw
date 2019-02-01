c6d8fa66ae95d64e0ea158558db607cb67dd0432
BILOMBO <44155422+BILOMBO@users.noreply.github.com>
2019-02-01 20:26:03 +0100

    Update form-jquery-validation.js

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -1,99 +1,114 @@
 
-$(document).ready(function () {
-    $("#gps").on("click", function (e) {
-        e.preventDefault();
+$( document ).ready(function() {
+    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+     console.log( "DOM ready!" );
+    
+     //ajout du compteur de nombre de caractéres pour chaque champ
+            $(document).keyup(function(){
+// ajout des compteurs de caractére a coté de chaque champs de saisie 
+    var nombreCaractere = $("#name").val().length;
+    var msg =  " " + nombreCaractere + ' Caractère(s)';
+    $('#compteur1').text(msg);
 
-    });
+    var nombreCaractere = $("#firstname").val().length;
+    var msg = " "+ nombreCaractere + ' Caractère(s)';
+    $('#compteur2').text(msg);
 
+    var nombreCaractere = $("#birth").val().length;
+    var msg =  " "+ nombreCaractere + ' Caractère(s)';
+    $('#compteur3').text(msg);
 
+    var nombreCaractere = $("#adresse").val().length;
+    var msg = " "+ nombreCaractere + ' Caractère(s)';
+    $('#compteur4').text(msg);
+    
+    var nombreCaractere = $("#mail").val().length;
+    var msg = " "+ nombreCaractere + ' Caractère(s)';
+    $('#compteur5').text(msg);
 
-    $("#valider").on("click", function (e) {
-        e.preventDefault();
+    // validation des champs avec un minimum de 5 
+   
 
+                valid = true ;
+                if($("#name").val().length < 5 ){
+                    $("#name").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#name").css("border-color","#48DE14");
+                }
+                if($("#firstname").val().length < 5 ){
+                    $("#firstname").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#firstname").css("border-color","#48DE14");
+                }
+                if($("#birth").val().length < 5 ){
+                    $("#birth").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#birth").css("border-color","#48DE14");
+                }
+                if($("#adresse").val().length < 5 ){
+                    $("#adresse").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#adresse").css("border-color","#48DE14");
+                }
+                if($("#mail").val().length < 5 ){
+                    $("#mail").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#mail").css("border-color","#48DE14");
+                }
 
+                return valid;
 
-        // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-        // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-        console.log("DOM ready!");
-
-        // Y mettre le code jQuery pour valider tous les champs du formulaire
-
-
-
-
-        if ($('#nom').val().length < 5 || $('#nom').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#myModal').modal("show");
-        }
-
-
-        else if ($('#prenom').val().length < 5 || $('#prenom').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#myModal').modal("show");
-        }
-
-
-        else if ($('#date').val().length < 5 || $('#date').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#myModal').modal("show");
-        }
-
-
-        else if ($('#adresse').val().length < 5 || $('#adresse').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#myModal').modal("show");
-        }
-        else if ($('#mail').val().length < 5 || $('#mail').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#myModal').modal("show");
-        }
-        else {
-
-
-            $("#valider").on("click", function (event) {
-                event.preventDefault();
-
-                var innom = document.getElementById("nom");
-                var inprenom = document.getElementById("prenom");
-                var inbirth = document.getElementById("date");
-                var inadresse = document.getElementById("adresse");
-                var inmail = document.getElementById("mail");
-
-
-                //stocker les valeurs saisie dans le navigateur
-                localStorage.setItem("nom", innom.value);
-                localStorage.setItem("prenom", inprenom.value);
-                localStorage.setItem("date", inbirth.value);
-                localStorage.setItem("adresse", inadresse.value);
-                localStorage.setItem("mail", inmail.value);
-
-                $('#success').addClass("alert alert-success").text("Sauvegarde réussie.");
-
-                $("#tablee").show();
-
-                // ajout des valeurs saisies dans le tableau
-                document.querySelector("table tbody").innerHTML = document.querySelector("table tbody")
-                    .innerHTML + '<tr><td>' + localStorage.getItem("nom") +
-                    '</td><td>' + localStorage.getItem("prenom") +
-                    '</td><td>' + localStorage.getItem("date") +
-                    '</td><td><a href="https://maps.google.com/maps?q=' + localStorage.getItem("adresse") + '">' + localStorage.getItem("adresse") + '</a></td><td><a href=mailto:>'
-                    + localStorage.getItem("mail") + '</a></td>';
-
-             
             });
 
 
+            $("#formulaire").on("submit",function store(event) { 
+                event.preventDefault();
+                
+                    var inputNom= document.getElementById("name");
+                    var inputPrenom= document.getElementById("firstname");
+                    var inputDn= document.getElementById("birth");
+                    var inputAdresse= document.getElementById("adresse");
+                    var inputEmail= document.getElementById("mail");
+                    
+                    if($("#nom").val() !== "" && $("#firstname").val() !== "" && $("#birth").val() !== "" 
+                  && $("#adresse").val() !== "" && $("#mail").val() !== "" ){
+                    
+                    //stocker les valeurs saisie dans le navigateur
+                    localStorage.setItem("name", inputNom.value);
+                    localStorage.setItem("firstname", inputPrenom.value);
+                    localStorage.setItem("birth", inputDn.value);
+                    localStorage.setItem("adresse", inputAdresse.value);
+                    localStorage.setItem("mail", inputEmail.value);
 
-        }
+                   $('#success').addClass("alert alert-success").text("le formulaire est sauvegardé dans le tableau");
 
-        var urls = document.querySelectorAll('.mapUrl');
+                  // $("#tablee").show();
+                   
+                   // ajout des valeurs saisie dans le tableau
+                   document.querySelector("table tbody").innerHTML = document.querySelector("table tbody")
+                   .innerHTML +'<tr><td>'+localStorage.getItem("name")+'</td><td>'+localStorage.getItem("firstname")
+                   
+                  
+                   +'</td><td>'+localStorage.getItem("birth")+'</td><td><a href="https://maps.google.com/maps?q='
+                   +localStorage.getItem("adresse")+'">'+localStorage.getItem("adresse")
+                  
+                   +'</a></td><td><a href=mailto:>'+localStorage.getItem("mail")+'</a></td>';
+                   }
+                  
+            });
+            
 
-        [].forEach.call(urls, function (item) {
-            // do whatever
-            item.setAttribute("href", "http://maps.google.com/maps?q=" + document.querySelector("#adresse").value);
-
-        });
-    });
-
-
-
-
-
-
+    
 
 });
